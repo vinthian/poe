fad5880f543d43746acd5ec899c28574e6fba20a
Nhat-Tan Duong <vinthian@gmail.com>
2013-11-18 01:00:15 -0500

    Refactored to have database variables stored in class.

--- poe/web_api/session.py
+++ poe/web_api/session.py
@@ -14,17 +14,17 @@ from poe.web_api.models import Base, StashTab, Item, get_or_create
 
 logger = logging.getLogger(__name__)
 
-engine = create_engine("sqlite:///%s" % settings["DB_PATH"])
-Base.metadata.bind = engine
-DBSession = sessionmaker()
-DBSession.bind = engine
-db = DBSession()
-
 
 class InvalidLoginException(Exception):
     def __init__(self, msg):
         super(InvalidLoginException, self).__init__(self, msg)
         logger.debug("Invalid login exception")
+
+
+class NotLoggedInException(Exception):
+    def __init__(self, msg):
+        super(NotLoggedInException, self).__init__(self, msg)
+        logger.debug("Not logged in exception")
 
 
 class DataNotFoundException(Exception):
@@ -41,18 +41,21 @@ class PathBrowser(mechanize.Browser, object):
     def open(self, *args, **kwargs):
         url = args[0]
         try:
-            logger.debug("%s %s" % (url.get_full_url(), url.get_method()))
+            logger.info("%s %s" % (url.get_full_url(), url.get_method()))
             if url.has_data():
-                logger.debug(urlparse.parse_qs(url.get_data()))
+                logger.info(urlparse.parse_qs(url.get_data()))
         except AttributeError:
-            logger.debug(url)
+            logger.info(url)
         return super(PathBrowser, self).open(*args, **kwargs)
 
 
 class PathSession(object):
-    def __init__(self, username, password):
-        self.username = username
+    def __init__(self):
         self.br = PathBrowser()
+        self.logged_in = False
+        self.initdb()
+
+    def login(self, username, password):
         self.br.open(settings["URL_LOGIN"])
         self.br.select_form(nr=0)
         self.br.form["login_email"] = username
@@ -61,8 +64,31 @@ class PathSession(object):
 
         self.br.open(settings["URL_ACCOUNT"])
         if self.br.geturl() != settings["URL_ACCOUNT"]:
-            raise InvalidLoginException("Could not log in")
+            self.logged_in = False
+            return False
+            # raise InvalidLoginException("Could not log in")
 
+        self.logged_in = True
+        return True
+
+    def initdb(self):
+        self.engine = create_engine("sqlite:///%s" % settings["DB_PATH"])
+        Base.metadata.bind = self.engine
+        DBSession = sessionmaker()
+        DBSession.bind = self.engine
+        self.db = DBSession()
+
+    def logged_in(func):
+        from functools import wraps
+        @wraps(func)
+        def wrapper(self, *args, **kwargs):
+            if self.logged_in:
+                return func(self, *args, **kwargs)
+            else:
+                raise NotLoggedInException("Not logged in")
+        return wrapper
+
+    @logged_in
     def get_stash(self, league="standard"):
         result = True
         tab_index = 0
@@ -75,7 +101,7 @@ class PathSession(object):
             items = []
 
             tab = get_or_create(
-                db,
+                self.db,
                 StashTab,
                 league=stash_tab["items"][0]["league"],
                 name=stash_tab["items"][0]["inventoryId"],
@@ -96,12 +122,13 @@ class PathSession(object):
                     x=item["x"],
                     y=item["y"]))
 
-            db.add(tab)
-            db.add_all(items)
-            db.commit()
+            self.db.add(tab)
+            self.db.add_all(items)
+            self.db.commit()
 
             tab_index += 1
 
+    @logged_in
     def get_stash_tab(self, league="standard", tab=0, delay=0):
         time.sleep(delay)
         url = settings["URL_STASH"] + ("?league=%s&tabIndex=%s" % (league, tab))
@@ -117,7 +144,7 @@ class PathSession(object):
         except ValueError:
             raise DataNotFoundException("Invalid data returned")
 
-        logger.debug(data)
+        logger.info(data)
 
         return data
 
@@ -129,7 +156,8 @@ if __name__ == "__main__":
     if sys.argv[1] == "initdb":
         if os.path.exists(settings["DB_PATH"]):
             os.remove(settings["DB_PATH"])
-        Base.metadata.create_all(engine)
+        session = PathSession(settings["USERNAME"], settings["PASSWORD"])
+        Base.metadata.create_all(session.engine)
 
     if sys.argv[1] == "get_stash":
         session = PathSession(settings["USERNAME"], settings["PASSWORD"])
